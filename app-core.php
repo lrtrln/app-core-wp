3e82b6fd5916236f0603feb5358b88bcc95a89f5
LRTRLN <lt@lrtrln.fr>
2024-09-27 14:09:21 +0200

    add resizeImage + svg improvement

--- app-core.php
+++ app-core.php
@@ -4,10 +4,11 @@
 Plugin Name:  Application Core
 Plugin URI:   https://lrtrln.fr
 Description:  Application core for custom CPT, config, menus, tax, etc.
-Version:      1.0.0
+Version:      1.1.0
 Author URI:   https://lrtrln.fr
 Text Domain:  lrtrln
 License:      GPL
+Requires PHP: 8.1
  */
 
 if (!defined('ABSPATH')) {
--- src/Classes/helpers.php
+++ src/Classes/helpers.php
@@ -1,9 +1,15 @@
 <?php
 
-function getError($data = null)
+/**
+ * Retrieves a string representation of the provided data.
+ *
+ * @param mixed $data The data to be processed. Can be a string, array, or object.
+ * @return string|null The string representation of the data or null if no data is provided.
+ */
+function getError(mixed $data = null): string | null
 {
     if ($data == null) {
-        return;
+        return null;
     }
 
     if (is_array($data) || is_object($data)) {
@@ -21,7 +27,7 @@ function getError($data = null)
  * @param mixed $log The data to be logged. Can be a string, array, or object.
  * @return void
  */
-function _log($data = null)
+function _log(mixed $data = null)
 {
     date_default_timezone_set('Europe/Paris');
     $date = date('h:i:s', time());
@@ -29,23 +35,41 @@ function _log($data = null)
     return error_log(sprintf('%s', getError($data)));
 }
 
-function _dd($data, $die = false)
+/**
+ * Dumps the provided data in a styled <pre> tag and optionally terminates the script.
+ *
+ * @param mixed $data The data to be dumped. Can be a string, array, or object.
+ * @param bool $die If true, the script will terminate after dumping the data. Default is false.
+ * @return void
+ */
+function _dd(mixed $data, bool $die = false): void
 {
     $class = "background:#333; color:white;border:1px dashed red; font-family: monospace; font-size: 12px; padding: 1em;";
+    $error = getError($data);
 
-    printf('<pre style="%s">%s</pre>', $class, getError($data));
+    print <<<HTML
+    <pre style="{$class}">
+        {$error}
+    </pre>
+    HTML;
 
-    if ($die === true ) {
+    if ($die === true) {
         die();
     }
 }
 
-function checkRegisteredActions($action)
+/**
+ * Checks if an action is registered in the WordPress action hook system.
+ *
+ * @param string $action The action to check for.
+ * @return bool True if the action is registered, false otherwise.
+ */
+function checkRegisteredActions(string $action): bool
 {
     global $wp_filter;
 
     if (isset($wp_filter[$action])) {
-        $actions = $wp_filter[$action];
+        $actions   = $wp_filter[$action];
         $callbacks = $actions->callbacks;
 
         _log($callbacks);
@@ -55,37 +79,152 @@ function checkRegisteredActions($action)
     }
 }
 
-function _svg($name)
+/**
+ * Retrieves the SVG path for a given name.
+ *
+ * @param string $name The name of the SVG.
+ * @return string|null The SVG path if found, or null if not found.
+ */
+function getSvgPath(string $name): string | null
 {
-    $svgs = [
-        'search'  => '<svg fill="currentColor" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m15.97 17.031c-1.479 1.238-3.384 1.985-5.461 1.985-4.697 0-8.509-3.812-8.509-8.508s3.812-8.508 8.509-8.508c4.695 0 8.508 3.812 8.508 8.508 0 2.078-.747 3.984-1.985 5.461l4.749 4.75c.146.146.219.338.219.531 0 .587-.537.75-.75.75-.192 0-.384-.073-.531-.22zm-5.461-13.53c-3.868 0-7.007 3.14-7.007 7.007s3.139 7.007 7.007 7.007c3.866 0 7.007-3.14 7.007-7.007s-3.141-7.007-7.007-7.007z" fill-rule="nonzero"/></svg>',
-        'contact' => '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm.021 17.824c-3.907 0-6.021-2.438-6.021-5.586 0-3.363 2.381-6.062 6.638-6.062 3.107 0 5.362 2.019 5.362 4.801 0 4.356-5.165 5.506-4.906 3.021-.354.555-.927 1.177-2.026 1.177-1.257 0-2.04-.92-2.04-2.403 0-2.222 1.461-4.1 3.19-4.1.829 0 1.399.438 1.638 1.11l.232-.816h1.169c-.122.416-1.161 4.264-1.161 4.264-.323 1.333.675 1.356 1.562.648 1.665-1.29 1.75-4.664-.499-6.071-2.411-1.445-7.897-.551-7.897 4.347 0 2.806 1.976 4.691 4.914 4.691 1.719 0 2.771-.465 3.648-.974l.588.849c-.856.482-2.231 1.104-4.391 1.104zm-1.172-7.153c-.357.67-.588 1.538-.588 2.212 0 1.805 1.761 1.816 2.626.12.356-.697.586-1.586.586-2.265 0-1.458-1.748-1.717-2.624-.067z"/></svg>',
-        'sep'     => '<svg xmlns="http://www.w3.org/2000/svg" width="9" height="11" viewBox="0 0 9 11"><g fill="#DDDEDF" fill-rule="evenodd"><path d="M3 0L4.8 0 1.8 10.8 0 10.8zM6.6 0L8.4 0 5.4 10.8 3.6 10.8z"></path></g></svg>',
-        'reading' => '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-journal-text" viewBox="0 0 16 16"><path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/><path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/><path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/></svg>',
-        'alert' => '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="var(--clr-primary)" class="bi bi-exclamation-triangle" viewBox="0 0 16 16"><path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/><path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z"/></svg>',
-        'alert2'   => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/></svg>',
-        'info'    => '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="var(--clr-primary)" class="bi bi-info-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg>',
-        'newsletter' => '<svg width="24" height="24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M24 23h-24v-13.275l2-1.455v-7.27h20v7.272l2 1.453v13.275zm-20-10.472v-9.528h16v9.527l-8 5.473-8-5.472zm14-.528h-12v-1h12v1zm0-3v1h-12v-1h12zm-7-1h-5v-3h5v3zm7 0h-6v-1h6v1zm0-2h-6v-1h6v1z"/></svg>',
-        'login' => '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="24" height="24" viewBox="0 0 24 24"><path d="M10 9.408l2.963 2.592-2.963 2.592v-1.592h-8v-2h8v-1.592zm-2-4.408v4h-8v6h8v4l8-7-8-7zm6-3c-1.787 0-3.46.474-4.911 1.295l.228.2 1.396 1.221c1.004-.456 2.114-.716 3.287-.716 4.411 0 8 3.589 8 8s-3.589 8-8 8c-1.173 0-2.283-.26-3.288-.715l-1.396 1.221-.228.2c1.452.82 3.125 1.294 4.912 1.294 5.522 0 10-4.477 10-10s-4.478-10-10-10z"/></svg>',
-        'page' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark" viewBox="0 0 16 16"><path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/></svg>',
-        'calendar' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16"><path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/></svg>',
-        'person' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/></svg>'
+    $svgPaths = include CONTENTS .'/svg/svg.php';
 
+    if (array_key_exists($name, $svgPaths)) {
+        return $svgPaths[$name];
+    }
+
+    return null;
+}
+
+/**
+ * Generates an SVG element with the specified attributes.
+ *
+ * @param string $name The name of the SVG to retrieve.
+ * @param array $args An associative array of attributes for the SVG element.
+ *                    - 'size' (int): The width and height of the SVG. Default is 24.
+ *                    - 'color' (string): The fill color of the SVG. Default is '#00000'.
+ *                    - 'class' (string): Additional CSS classes to apply to the SVG element.
+ *                    - 'viewbox' (int): The viewBox attribute of the SVG. Default is 24.
+ * @return string The generated SVG element as a string.
+ */
+function _svg($name, $args = [])
+{
+    $defaults = [
+        'size' => 24,
+        'color' => '#00000',
+        'class' => '',
+        'viewbox' => 24,
     ];
 
-    if (array_key_exists($name, $svgs)) {
-        return $svgs[$name];
-    } else {
+    $args = array_merge($defaults, $args);
+    $svg = getSvgPath($name);
+
+    if ($svg === null) {
         return null;
     }
+
+    return <<<HTML
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="{$args['size']}"
+            height="{$args['size']}"
+            viewBox="0 0 {$args['viewbox']} {$args['viewbox']}"
+            style="fill: {$args['color']}; --darkreader-inline-fill: #ffffff;"
+            data-darkreader-inline-fill=""
+            class="{$args['class']}"
+        >
+            {$svg}
+        </svg>
+    HTML;
 }
 
-function getExcerpt($lenght = 15)
+
+/**
+ * Retrieves an excerpt of the specified length.
+ *
+ * @param int $length The number of words to include in the excerpt. Default is 15.
+ * @return string The trimmed excerpt.
+ */
+function getExcerpt(int $lenght = 15): string
 {
-    return wp_trim_words(get_the_excerpt(), $lenght);
+    return wp_trim_words(
+        get_the_excerpt(),
+        $lenght
+    );
 }
 
+/**
+ * Checks if the current environment is a development environment.
+ *
+ * @return bool True if the environment is 'dev', false otherwise.
+ */
 function isDev(): bool
 {
     return defined('WP_ENV') && WP_ENV === 'dev';
+}
+
+/**
+ * Resizes an image to the specified width and height, and caches the result.
+ *
+ * @param string $imagePath The path to the original image.
+ * @param int $width The desired width of the resized image.
+ * @param int $height The desired height of the resized image.
+ * @return string|false The URL of the cached resized image, or false on failure.
+ */
+function resizeImage(string $imagePath, int $width, int $height): string | false
+{
+    $cacheDir    =  sprintf('%s/cache/', wp_upload_dir()['basedir']);
+    $cachedImage = sprintf('%s%sx%s_%s.webp',
+        $cacheDir,
+        $width,
+        $height,
+        pathinfo($imagePath, PATHINFO_FILENAME)
+    );
+
+    if (file_exists($cachedImage)) {
+        return wp_upload_dir()['baseurl'] . '/cache/' . basename($cachedImage);
+    }
+
+    $originalImagePath = wp_upload_dir()['basedir'] . '/' . $imagePath;
+    list(
+        $originalWidth,
+        $originalHeight,
+        $imageType) = getimagesize($originalImagePath);
+
+    $newImage = imagecreatetruecolor($width, $height);
+
+    switch ($imageType) {
+        case IMAGETYPE_JPEG:
+            $source = imagecreatefromjpeg($originalImagePath);
+            break;
+        case IMAGETYPE_PNG:
+            $source = imagecreatefrompng($originalImagePath);
+            break;
+        case IMAGETYPE_GIF:
+            $source = imagecreatefromgif($originalImagePath);
+            break;
+        default:
+            return false;
+    }
+
+    imagecopyresampled(
+        $newImage,
+        $source,
+        0, 0, 0, 0,
+        $width,
+        $height,
+        $originalWidth,
+        $originalHeight
+    );
+
+    if (!file_exists($cacheDir)) {
+        mkdir($cacheDir, 0755, true);
+    }
+
+    imagewebp($newImage, $cachedImage, 90);
+
+    imagedestroy($newImage);
+    imagedestroy($source);
+
+    return wp_upload_dir()['baseurl'] . '/cache/' . basename($cachedImage);
 }
